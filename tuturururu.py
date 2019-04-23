0783cdbb57ebb0417501541c4afbe94750cf9a7a
insidemirage <vladislavlightinthedarkness@gmail.com>
2019-04-23 22:06:03 +0300

    first stable version(proj only, btw no db cr)

--- tuturururu.py
+++ tuturururu.py
@@ -19,24 +19,35 @@ class Program(object):
         self.frame = []
         self.btnadd = Button(text='Add exchange')
         self.exch = []
+        self.debug = ['https://kwork.ru/projects?c=15&','https://kwork.ru/projects']
         #connecting to db
-        if os.path.isfile('data.db'):
+        if os.path.isfile('data.sadb'):
             self.connection = sqlite3.connect('data.db')
             self.cursor = self.connection.cursor()
             self.cursor.execute("""SELECT * FROM logdata""")
             self.data = self.cursor.fetchall()
+            for i in range(0, len(self.data)):
+                self.frame.append(Frame(self.root,bd=1,bg='white', relief=RAISED))
+                self.labelmessage.append(Label(self.frame[i], text='Messages: 0',bg='white', justify='left'))
+                self.labelproj.append(Label(self.frame[i], text='Projects: 0',bg='white', justify='left'))
+                self.labelmessage[i].pack(anchor='nw')            
+                self.labelproj[i].pack(anchor='nw')
+                self.frame[i].pack(fill='x', ipady=5)
+                self.exch.append(Exchange('https://kwork.ru/api/user/login', self.data[i][1], self.data[i][2], self.debug[i], 'wants-card__header-title',self.labelproj[i], 60))
+
         else:
             print('Error db not found')
-            sys.exit(0)
-        for i in range(0, len(self.data)):
-            self.frame.append(Frame(self.root,bd=1,bg='white', relief=RAISED))
-            self.labelmessage.append(Label(self.frame[i], text='Messages: 0',bg='white', justify='left'))
-            self.labelproj.append(Label(self.frame[i], text='Projects: 0',bg='white', justify='left'))
-            self.labelmessage[i].pack(anchor='nw')            
-            self.labelproj[i].pack(anchor='nw')
-            self.frame[i].pack(fill='x', ipady=5)
-        self.exch.append(Exchange('https://kwork.ru/api/user/login', 'ksajfkq214@yandex.ru', '84j27696R3', 'https://kwork.ru/projects', 'wants-card__header-title',self.labelproj[0], 10))
-        self.exch.append(Exchange('https://kwork.ru/api/user/login', 'kikstarter', 'safv231vas', 'https://kwork.ru/projects', 'wants-card__header-title',self.labelproj[1], 10))
+            top = Toplevel(self.root)
+            top.geometry('400x100')
+            Label(top,bg='white',text='Привет. На вашем пк на данный момент отсутствует база данных\n предназначенная для хранения данных об аккаунтах\n для этого приложения').pack()
+            Button(top, text='ОК', width=5, command=top.destroy).pack()
+            top.iconbitmap('icon.ico')
+            top.configure(bg='white')
+            top.grab_set()
+            top.focus_set()
+            top.wait_window()
+            
+            # sys.exit(0)
         self.root.mainloop()
 
 
@@ -80,6 +91,3 @@ class Exchange(object):
         self.updtimer = Timer(self.updtime, self.Update)
         self.updtimer.start()
 ex = Program()
-kwork = []
-# kwork.append(Exchange('https://kwork.ru/api/user/login', 'ksajfkq214@yandex.ru', '84j27696R3', 'https://kwork.ru/projects', 'wants-card__header-title', 10))
-# kwork.append(Exchange('https://kwork.ru/api/user/login', 'kikstarter', 'safv231vas', 'https://kwork.ru/projects', 'wants-card__header-title', 10))

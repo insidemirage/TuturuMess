8f9196d520bf94cb846c0b3da3b21f7e4eeef51e
insidemirage <vladislavlightinthedarkness@gmail.com>
2019-04-24 00:00:19 +0300

    Modals

--- tuturururu.py
+++ tuturururu.py
@@ -11,16 +11,42 @@ class Program(object):
     def __init__(self):
         self.root = Tk()
         self.root.title("Tuturu Mess")
-        self.root.geometry("300x400")
+        x = int((self.root.winfo_screenwidth() - 250) / 2)
+        y = int((self.root.winfo_screenheight() - 400) / 2)
+        self.root.geometry("250x400+{0}+{1}".format(x,y))
         self.root.configure(background='white')
         self.root.iconbitmap('icon.ico')
+        self.root.resizable(width=False, height=False)
         self.labelmessage = []
         self.labelproj = []
         self.frame = []
         self.btnadd = Button(text='Add exchange')
         self.exch = []
         self.debug = ['https://kwork.ru/projects?c=15&','https://kwork.ru/projects']
+        self.txt = []
+        
+        
+        
         #connecting to db
+        if (not os.path.isfile('rsu.uis')):
+            top = Toplevel(self.root)
+            top.transient(self.root)
+            x = int((self.root.winfo_screenwidth() - 300) / 2)
+            y = int((self.root.winfo_screenheight() - 100) / 2)
+            top.protocol("WM_DELETE_WINDOW", self.root.destroy)
+            top.geometry("300x100+{0}+{1}".format(x,y))
+            
+            Label(top,bg='white',text='На вашем компьтере отсутствуют файлы с текстом').pack(fill='x')
+            Button(top, text='ОК', width=5, command=self.root.destroy).pack()
+            top.iconbitmap('icon.ico')
+            top.configure(bg='white')
+            top.grab_set()
+            top.focus_set()
+            top.resizable(width=False, height=False)
+            top.wait_window()
+        else:
+            with open('ru.uis', encoding='utf-8') as txtsrc:
+                self.txt = txtsrc.readlines()
         if os.path.isfile('data.sadb'):
             self.connection = sqlite3.connect('data.db')
             self.cursor = self.connection.cursor()
@@ -37,15 +63,19 @@ class Program(object):
 
         else:
             print('Error db not found')
-            top = Toplevel(self.root)
-            top.geometry('400x100')
-            Label(top,bg='white',text='Привет. На вашем пк на данный момент отсутствует база данных\n предназначенная для хранения данных об аккаунтах\n для этого приложения').pack()
-            Button(top, text='ОК', width=5, command=top.destroy).pack()
-            top.iconbitmap('icon.ico')
-            top.configure(bg='white')
-            top.grab_set()
-            top.focus_set()
-            top.wait_window()
+            if(self.txt):
+                top = Toplevel(self.root)
+                top.geometry('400x190')
+                Label(top,bg='white',text=self.txt[0],wraplength=300).pack(fill='x')
+                Label(top,bg='white',text=self.txt[1]).pack(fill='x')
+                Label(top,bg='white',text=self.txt[2]).pack(fill='x')
+                Button(top, text='ОК', width=10, command=top.destroy).pack()
+                top.iconbitmap('icon.ico')
+                top.configure(bg='white')
+                top.grab_set()
+                top.focus_set()
+                top.resizable(width=False, height=False)
+                top.wait_window()
             
             # sys.exit(0)
         self.root.mainloop()
